1cd7bd453b072225f15482e06af0edff6681852a
AlphaBril <megack79@gmail.com>
2021-08-14 13:04:50 +0200

    added trees and going to add  plane and change axis

--- src/main/webapp/app/engine/engine.service.ts
+++ src/main/webapp/app/engine/engine.service.ts
@@ -2,23 +2,23 @@ import { WindowRefService } from './../services/window-ref.service';
 import { ElementRef, Injectable, NgZone } from '@angular/core';
 import {
   Engine,
-  FreeCamera,
   Scene,
   Light,
   MeshBuilder,
   Mesh,
   SceneLoader,
-  Color3,
   Color4,
   Vector3,
   HemisphericLight,
   StandardMaterial,
   Texture,
-  DynamicTexture,
   Animation,
   Space,
   AbstractMesh,
   Layer,
+  Color3,
+  FlyCamera,
+  Plane,
 } from '@babylonjs/core';
 import '@babylonjs/loaders/glTF';
 
@@ -26,13 +26,14 @@ import '@babylonjs/loaders/glTF';
 export class EngineService {
   private canvas!: HTMLCanvasElement;
   private engine!: Engine;
-  private camera!: FreeCamera;
+  private camera!: FlyCamera;
   private scene!: Scene;
   private light!: Light;
   private layer!: Layer;
 
   private branch!: Mesh;
   private koodibril!: AbstractMesh;
+  private trees!: AbstractMesh[];
 
   public constructor(private ngZone: NgZone, private windowRef: WindowRefService) {}
 
@@ -48,24 +49,46 @@ export class EngineService {
     this.scene.clearColor = new Color4(0, 0, 0, 0);
 
     // create a FreeCamera, and set its position to (x:5, y:10, z:-20 )
-    this.camera = new FreeCamera('camera1', new Vector3(0, 0, -10), this.scene);
+    this.camera = new FlyCamera('camera1', new Vector3(0, 0, -10), this.scene);
+    this.camera.rotation.x = Math.PI / 2;
 
     // target the camera to scene origin
     this.camera.setTarget(Vector3.Zero());
 
     // create a basic light, aiming 0,1,0 - meaning, to the sky
     this.light = new HemisphericLight('light1', new Vector3(0, 1, 0), this.scene);
-    this.layer = new Layer('', '../../content/images/jungle.jpg', this.scene, true);
+    const abstractPlane = Plane.FromPositionAndNormal(new Vector3(0, 0, 0), new Vector3(1, 0, 0));
+    const plane = MeshBuilder.CreatePlane('plane', { sourcePlane: abstractPlane });
+    plane.position.x = 0;
+    plane.position.y = 0;
+    plane.position.z = 0;
+    plane.rotation.x = 0;
+    plane.rotation.y = 0;
+    plane.rotation.z = 0;
+    //this.layer = new Layer('', '../../content/images/jungle.jpg', this.scene, true);
+
+    this.trees = [];
+    for (let i = 1; i <= 9; i++) {
+      const tree = await SceneLoader.ImportMeshAsync('', '../../content/assets/models/', 'tree' + i.toString() + '.glb', this.scene);
+      tree.meshes[0].scaling.scaleInPlace(3);
+      tree.meshes[0].rotate(new Vector3(0, 1, 0), 1.5 * Math.PI);
+      tree.meshes[0].position.x = 0;
+      tree.meshes[0].position.y = 0;
+      tree.meshes[0].position.z = 0;
+      this.trees.push(tree.meshes[0]);
+    }
+    this.forest();
+    this.showWorldAxis(5);
 
     // create a built-in "branch" shape; its constructor takes 4 params: name, subdivisions, radius, scene
-    this.branch = MeshBuilder.CreateDisc('disc', { radius: 0.001 });
-    const imported = await SceneLoader.ImportMeshAsync('', '../../content/assets/models/', 'koodibril.glb', this.scene);
+    this.branch = MeshBuilder.CreateDisc('disc', { radius: 0.1 });
+    const colibri = await SceneLoader.ImportMeshAsync('', '../../content/assets/models/', 'koodibril.glb', this.scene);
     const koodibrilAnim = this.scene.getAnimationGroupByName('fly');
     if (koodibrilAnim) {
-      imported.animationGroups[0].stop();
+      colibri.animationGroups[0].stop();
       koodibrilAnim.start(true, 10.0, koodibrilAnim.from, koodibrilAnim.to, false);
     }
-    this.koodibril = imported.meshes[0];
+    this.koodibril = colibri.meshes[0];
     // create the material with its texture for the branch and assign it to the branch
     const branchMaterial = new StandardMaterial('sun_surface', this.scene);
     branchMaterial.diffuseTexture = new Texture('../../content/assets/textures/sun.jpg', this.scene);
@@ -79,9 +102,7 @@ export class EngineService {
     this.koodibril.position.y = 1;
     this.koodibril.position.z = 0;
     this.koodibril.parent = this.branch;
-    console.log(this.koodibril.rotationQuaternion);
     this.koodibril.rotate(new Vector3(0, 1, 0), 1.5 * Math.PI);
-    console.log(this.koodibril.rotationQuaternion);
     this.fly();
   }
 
@@ -179,5 +200,71 @@ export class EngineService {
     flyAnimate.onAnimationEndObservable.add(() => {
       this.fly();
     });
+  }
+
+  public forest(): void {
+    const treeFront1 = this.trees[Math.floor(Math.random() * (9 - 1) + 1)];
+    const treeFront2 = this.trees[Math.floor(Math.random() * (9 - 1) + 1)];
+    const treeMiddle1 = this.trees[Math.floor(Math.random() * (9 - 1) + 1)];
+    const treeMiddle2 = this.trees[Math.floor(Math.random() * (9 - 1) + 1)];
+    const treeMiddle3 = this.trees[Math.floor(Math.random() * (9 - 1) + 1)];
+    const treeBack1 = this.trees[Math.floor(Math.random() * (9 - 1) + 1)];
+    const treeBack2 = this.trees[Math.floor(Math.random() * (9 - 1) + 1)];
+    const treeBack3 = this.trees[Math.floor(Math.random() * (9 - 1) + 1)];
+    const treeBack4 = this.trees[Math.floor(Math.random() * (9 - 1) + 1)];
+    treeFront1.position.x = -5;
+    treeFront1.position.y = 0;
+    treeFront2.position.x = 5;
+    treeFront2.position.y = 0;
+  }
+
+  public showWorldAxis(size: number): void {
+    // var makeTextPlane = function(text, color, size) {
+    //     var dynamicTexture = new DynamicTexture("DynamicTexture", 50, scene, true);
+    //     dynamicTexture.hasAlpha = true;
+    //     dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color , "transparent", true);
+    //     var plane = Mesh.CreatePlane("TextPlane", size, scene, true);
+    //     plane.material = new StandardMaterial("TextPlaneMaterial", scene);
+    //     plane.material.backFaceCulling = false;
+    //     plane.material.specularColor = new Color3(0, 0, 0);
+    //     plane.material.diffuseTexture = dynamicTexture;
+    // return plane;
+    //  };
+    const axisX = Mesh.CreateLines(
+      'axisX',
+      [
+        Vector3.Zero(),
+        new Vector3(size, 0, 0),
+        new Vector3(size * 0.95, 0.05 * size, 0),
+        new Vector3(size, 0, 0),
+        new Vector3(size * 0.95, -0.05 * size, 0),
+      ],
+      this.scene
+    );
+    axisX.color = new Color3(1, 0, 0);
+    const axisY = Mesh.CreateLines(
+      'axisY',
+      [
+        Vector3.Zero(),
+        new Vector3(0, size, 0),
+        new Vector3(-0.05 * size, size * 0.95, 0),
+        new Vector3(0, size, 0),
+        new Vector3(0.05 * size, size * 0.95, 0),
+      ],
+      this.scene
+    );
+    axisY.color = new Color3(0, 1, 0);
+    const axisZ = Mesh.CreateLines(
+      'axisZ',
+      [
+        Vector3.Zero(),
+        new Vector3(0, 0, size),
+        new Vector3(0, -0.05 * size, size * 0.95),
+        new Vector3(0, 0, size),
+        new Vector3(0, 0.05 * size, size * 0.95),
+      ],
+      this.scene
+    );
+    axisZ.color = new Color3(0, 0, 1);
   }
 }
