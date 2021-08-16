5014a976a193d142f5882cccf6e0c1ef82079e73
AlphaBril <megack79@gmail.com>
2021-08-16 12:29:27 +0200

    added random generation and corredted colibri follow

--- src/main/webapp/app/engine/engine.service.ts
+++ src/main/webapp/app/engine/engine.service.ts
@@ -16,6 +16,7 @@ import {
   Layer,
   Color3,
   FlyCamera,
+  StandardMaterial,
 } from '@babylonjs/core';
 import '@babylonjs/loaders/glTF';
 
@@ -33,6 +34,7 @@ export class EngineService {
   private koodibril!: AbstractMesh;
   private trees!: AbstractMesh[];
   private bushes!: AbstractMesh[];
+  private forest!: AbstractMesh[];
 
   public constructor(private ngZone: NgZone, private windowRef: WindowRefService) {}
 
@@ -74,19 +76,22 @@ export class EngineService {
 
     this.trees = [];
     this.bushes = [];
+    this.forest = [];
     for (let i = 1; i <= 9; i++) {
       const tree = await SceneLoader.ImportMeshAsync('', '../../content/assets/models/', 'tree' + i.toString() + '.glb', this.scene);
       tree.meshes[0].scaling.scaleInPlace(2.5);
       tree.meshes[0].rotate(new Vector3(0, 1, 0), 1.5 * Math.PI);
+      tree.meshes[0].position = new Vector3(-10, 0, -10);
       this.trees.push(tree.meshes[0]);
       if (i <= 4) {
         const bush = await SceneLoader.ImportMeshAsync('', '../../content/assets/models/', 'bush' + i.toString() + '.glb', this.scene);
         bush.meshes[0].scaling.scaleInPlace(2.5);
         bush.meshes[0].rotate(new Vector3(0, 1, 0), 1.5 * Math.PI);
+        bush.meshes[0].position = new Vector3(-10, 0, -10);
         this.bushes.push(bush.meshes[0]);
       }
     }
-    this.forest();
+    this.seed();
 
     // create a built-in "branch" shape; its constructor takes 4 params: name, subdivisions, radius, scene
     this.branch = MeshBuilder.CreateDisc('disc', { radius: 0.1 });
@@ -98,7 +103,7 @@ export class EngineService {
     }
     this.koodibril = colibri.meshes[0];
     // create the material with its texture for the branch and assign it to the branch
-    this.branch.position.y = 0;
+    this.branch.position.y = 2;
     this.branch.position.z = 0;
     this.koodibril.scaling.scaleInPlace(0.2);
     this.koodibril.position.y = 1;
@@ -124,10 +129,8 @@ export class EngineService {
         });
       }
 
-      const offsetx = this.canvas.width / 200;
-      const offsety = this.canvas.height / 200;
       this.canvas.addEventListener('mouseout', () => {
-        const translateVector = new Vector3(-this.branch.position.x, -this.branch.position.y, 0);
+        const translateVector = new Vector3(-this.branch.position.x, -this.branch.position.y + 2, 0);
         const distance = translateVector.length();
 
         const direction = new Vector3(translateVector.x, translateVector.y, translateVector.z);
@@ -143,8 +146,10 @@ export class EngineService {
       });
 
       this.canvas.addEventListener('mousemove', () => {
-        const x = this.scene.pointerX / 100 - offsetx;
-        const y = -this.scene.pointerY / 100 + offsety;
+        const offsetCanvasx = this.canvas.width / 200;
+        const offsetCanvasy = this.canvas.height / 200;
+        const x = (this.scene.pointerX / 100 - offsetCanvasx) / 2;
+        const y = (-this.scene.pointerY / 100 + offsetCanvasy + 3) / 1.5;
         this.branch.position.x = x;
         this.branch.position.y = y;
       });
@@ -152,8 +157,10 @@ export class EngineService {
       this.canvas.addEventListener('wheel', event => {
         const delta = Math.sign(event.deltaY);
         if (!this.move) {
+          this.addRow(delta);
           this.move = true;
-          this.trees.forEach(element => {
+          let rollOver: any;
+          this.forest.forEach(element => {
             const frameRate = 10;
             const zkeyFrames = [
               {
@@ -162,31 +169,16 @@ export class EngineService {
               },
               {
                 frame: frameRate,
-                value: element.position.z + 5 * delta,
+                value: element.position.z + 4 * delta,
               },
             ];
             const zSlide = new Animation('zSlide', 'position.z', frameRate, Animation.ANIMATIONTYPE_FLOAT);
             zSlide.setKeys(zkeyFrames);
-            this.scene.beginDirectAnimation(element, [zSlide], 0, 2 * frameRate, false, 2);
+            rollOver = this.scene.beginDirectAnimation(element, [zSlide], 0, 2 * frameRate, false, 2);
           });
-          this.bushes.forEach(element => {
-            const frameRate = 10;
-            const zkeyFrames = [
-              {
-                frame: 0,
-                value: element.position.z,
-              },
-              {
-                frame: frameRate,
-                value: element.position.z + 5 * delta,
-              },
-            ];
-            const zSlide = new Animation('zSlide', 'position.z', frameRate, Animation.ANIMATIONTYPE_FLOAT);
-            zSlide.setKeys(zkeyFrames);
-            const rollOver = this.scene.beginDirectAnimation(element, [zSlide], 0, 2 * frameRate, false, 2);
-            rollOver.onAnimationEndObservable.add(() => {
-              this.move = false;
-            });
+          rollOver!.onAnimationEndObservable.add(() => {
+            this.move = false;
+            this.deleteRow(delta);
           });
         }
       });
@@ -242,78 +234,61 @@ export class EngineService {
     });
   }
 
-  public forest(): void {
-    for (let i = this.trees.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.trees[i], this.trees[j]] = [this.trees[j], this.trees[i]];
-    }
-    const treeFront1 = this.trees[0];
-    const treeFront2 = this.trees[1];
-    const treeMiddle1 = this.trees[2];
-    const treeMiddle2 = this.trees[3];
-    const treeMiddle3 = this.trees[4];
-    const treeBack1 = this.trees[5];
-    const treeBack2 = this.trees[6];
-    const treeBack3 = this.trees[7];
-    const treeBack4 = this.trees[8];
+  public seed(): void {
+    this.shuffleMesh(this.trees);
+    const treeFront1 = this.trees[0].clone('', null);
+    const treeFront2 = this.trees[1].clone('', null);
+    const treeMiddle1 = this.trees[2].clone('', null);
+    const treeMiddle2 = this.trees[3].clone('', null);
+    const treeMiddle3 = this.trees[4].clone('', null);
+    const treeBack1 = this.trees[5].clone('', null);
+    const treeBack2 = this.trees[6].clone('', null);
+    const treeBack3 = this.trees[7].clone('', null);
+    const treeBack4 = this.trees[8].clone('', null);
 
-    treeFront1.position.x = -3;
-    treeFront1.position.z = 0;
+    treeFront1!.position.x = -3;
+    treeFront1!.position.z = 0;
 
-    treeFront2.position.x = 4;
-    treeFront2.position.z = 0;
+    treeFront2!.position.x = 4;
+    treeFront2!.position.z = 0;
 
-    treeMiddle1.position.x = -6;
-    treeMiddle1.position.z = 4;
+    treeMiddle1!.position.x = -6;
+    treeMiddle1!.position.z = 4;
 
-    treeMiddle2.position.x = 3;
-    treeMiddle2.position.z = 4;
+    treeMiddle2!.position.x = 3;
+    treeMiddle2!.position.z = 4;
 
-    treeMiddle3.position.x = 5;
-    treeMiddle3.position.z = 4;
+    treeMiddle3!.position.x = 5;
+    treeMiddle3!.position.z = 4;
 
-    treeBack1.position.x = -4;
-    treeBack1.position.z = 8;
+    treeBack1!.position.x = -4;
+    treeBack1!.position.z = 8;
 
-    treeBack2.position.x = -3;
-    treeBack2.position.z = 8;
+    treeBack2!.position.x = -3;
+    treeBack2!.position.z = 8;
 
-    treeBack3.position.x = 2;
-    treeBack3.position.z = 8;
+    treeBack3!.position.x = 2;
+    treeBack3!.position.z = 8;
 
-    treeBack4.position.x = 5;
-    treeBack4.position.z = 8;
+    treeBack4!.position.x = 5;
+    treeBack4!.position.z = 8;
 
-    for (let i = this.bushes.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.bushes[i], this.bushes[j]] = [this.bushes[j], this.bushes[i]];
-    }
-    const bushFront1 = this.bushes[0]; // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-    const bushFront2 = this.bushes[1]; // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-    const bushFront3 = this.bushes[2]; // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-    const bushFront4 = this.bushes[3]; // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-    bushFront1.position.x = -5;
-    bushFront1.position.z = 0;
-    bushFront2.position.x = 1;
-    bushFront2.position.z = 0;
-    bushFront3.position.x = -1;
-    bushFront3.position.z = 0;
-    bushFront4.position.x = 5;
-    bushFront4.position.z = 0;
+    this.forest.push(treeFront1!);
+    this.forest.push(treeFront2!);
+    this.forest.push(treeMiddle1!);
+    this.forest.push(treeMiddle2!);
+    this.forest.push(treeMiddle3!);
+    this.forest.push(treeBack1!);
+    this.forest.push(treeBack2!);
+    this.forest.push(treeBack3!);
+    this.forest.push(treeBack4!);
 
-    for (let z = 4; z <= 8; z = z + 4) {
-      for (let i = this.bushes.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.bushes[i], this.bushes[j]] = [this.bushes[j], this.bushes[i]];
-      }
+    for (let z = 0; z <= 8; z = z + 4) {
+      this.shuffleMesh(this.bushes);
       const cloneBush1 = this.bushes[0].clone('', null); // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-      this.bushes.push(cloneBush1!);
       const cloneBush2 = this.bushes[1].clone('', null); // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-      this.bushes.push(cloneBush2!);
       const cloneBush3 = this.bushes[2].clone('', null); // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-      this.bushes.push(cloneBush3!);
       const cloneBush4 = this.bushes[3].clone('', null); // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-      this.bushes.push(cloneBush4!);
       cloneBush1!.position.x = -5;
       cloneBush1!.position.z = z;
       cloneBush2!.position.x = 1;
@@ -322,6 +297,85 @@ export class EngineService {
       cloneBush3!.position.z = z;
       cloneBush4!.position.x = 5;
       cloneBush4!.position.z = z;
+      this.forest.push(cloneBush1!);
+      this.forest.push(cloneBush2!);
+      this.forest.push(cloneBush3!);
+      this.forest.push(cloneBush4!);
     }
+  }
+
+  public shuffleMesh(array: Array<AbstractMesh>): Array<AbstractMesh> {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  public shuffleArray(array: Array<number>): Array<number> {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  public addRow(delta: number): void {
+    this.shuffleMesh(this.trees);
+    const newTrees = Math.floor(Math.random() * (4 - 2) + 2);
+    const tree_positon = [-3, 4, -6, 3, 5, 2];
+    const position = this.shuffleArray(tree_positon);
+    for (let i = 0; i < newTrees; i++) {
+      const newTree = this.trees[i].clone('', null);
+      newTree!.position.x = tree_positon[i];
+      newTree!.position.z = delta === -1 ? 12 : -4;
+      this.forest.push(newTree!);
+    }
+    this.shuffleMesh(this.bushes);
+    const cloneBush1 = this.bushes[0].clone('', null);
+    const cloneBush2 = this.bushes[1].clone('', null);
+    const cloneBush3 = this.bushes[2].clone('', null);
+    const cloneBush4 = this.bushes[3].clone('', null);
+    cloneBush1!.position.x = -5;
+    cloneBush1!.position.z = delta === -1 ? 12 : -4;
+    cloneBush2!.position.x = 1;
+    cloneBush2!.position.z = delta === -1 ? 12 : -4;
+    cloneBush3!.position.x = -1;
+    cloneBush3!.position.z = delta === -1 ? 12 : -4;
+    cloneBush4!.position.x = 5;
+    cloneBush4!.position.z = delta === -1 ? 12 : -4;
+    this.forest.push(cloneBush1!);
+    this.forest.push(cloneBush2!);
+    this.forest.push(cloneBush3!);
+    this.forest.push(cloneBush4!);
+  }
+
+  public deleteRow(delta: number): void {
+    this.forest.forEach(element => {
+      if ((element.position.z === 12 && delta === 1) || (element.position.z === -4 && delta === -1)) {
+        // const frameRate = 10;
+        // console.log(element.material!.alpha);
+        // const alphakeyFrames = [
+        //   {
+        //     frame: 0,
+        //     value: element.material!.alpha,
+        //   },
+        //   {
+        //     frame: frameRate,
+        //     value: 0,
+        //   }
+        // ];
+        // const alphaDown = new Animation('alphaDown', 'material.alpha', frameRate, Animation.ANIMATIONTYPE_FLOAT);
+
+        // alphaDown.setKeys(alphakeyFrames);
+        // const animations = [alphaDown];
+        // const goDown = this.scene.beginDirectAnimation(element, animations, 0, frameRate, false, 1);
+        // goDown.onAnimationEndObservable.add(() => {
+        //   console.log(element.material!.alpha);
+        //   element.dispose();
+        // });
+        element.dispose();
+      }
+    });
   }
 }
