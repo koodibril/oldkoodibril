e620a1110660a14529549a07f2d44889805977fe
AlphaBril <megack79@gmail.com>
2021-08-18 19:52:36 +0200

    updated bushes for color, changed way of importation for bushed and tree

--- src/main/webapp/app/engine/engine.service.ts
+++ src/main/webapp/app/engine/engine.service.ts
@@ -17,10 +17,7 @@ import {
   Color3,
   FlyCamera,
   ISceneLoaderAsyncResult,
-  IParticleSystem,
   AnimationGroup,
-  ParticleSystem,
-  Texture,
   Material,
 } from '@babylonjs/core';
 import '@babylonjs/loaders/glTF';
@@ -40,15 +37,15 @@ export class EngineService {
 
   private branch!: Mesh;
   private koodibril!: AbstractMesh;
-  private trees!: AbstractMesh[];
-  private bushes!: AbstractMesh[];
   private forest!: AbstractMesh[];
   private leftoright!: boolean;
   private timeout!: boolean;
   private flowers!: any[];
+  private bushes!: any[];
+  private trees!: any[];
   private open!: boolean;
-  private particle!: IParticleSystem;
   private koodibrilAnim!: AnimationGroup[];
+  private loading!: boolean;
 
   public constructor(private ngZone: NgZone, private windowRef: WindowRefService) {}
 
@@ -83,28 +80,15 @@ export class EngineService {
     ground.material.backFaceCulling = false;
 
     this.trees = [];
-    this.bushes = [];
     this.forest = [];
     this.flowers = [];
-    for (let i = 1; i <= 9; i++) {
-      const tree = await SceneLoader.ImportMeshAsync('', '../../content/assets/models/', 'tree' + i.toString() + '.glb', this.scene);
-      tree.meshes[0].scaling.scaleInPlace(2.5);
-      tree.meshes[0].rotate(new Vector3(0, 1, 0), 1.5 * Math.PI);
-      tree.meshes[0].position = new Vector3(-10, 0, -10);
-      this.trees.push(tree.meshes[0]);
-      if (i <= 4) {
-        const bush = await SceneLoader.ImportMeshAsync('', '../../content/assets/models/', 'bush' + i.toString() + '.glb', this.scene);
-        bush.meshes[0].scaling.scaleInPlace(2.5);
-        bush.meshes[0].rotate(new Vector3(0, 1, 0), 1.5 * Math.PI);
-        bush.meshes[0].position = new Vector3(-10, 0, -10);
-        this.bushes.push(bush.meshes[0]);
-      }
-    }
+    this.bushes = [];
     this.seed();
 
     // create a built-in "branch" shape; its constructor takes 4 params: name, subdivisions, radius, scene
-    this.branch = MeshBuilder.CreateDisc('disc', { radius: 0.1 });
+    this.branch = MeshBuilder.CreateDisc('disc', { radius: 0.001 });
     const colibri = await SceneLoader.ImportMeshAsync('', '../../content/assets/models/', 'koodibril.glb', this.scene);
+
     colibri.animationGroups[0].stop();
     colibri.animationGroups[0].start(true, 10.0);
     this.koodibril = colibri.meshes[0];
@@ -121,6 +105,7 @@ export class EngineService {
     this.leftoright = true;
     this.timeout = false;
     this.open = false;
+    this.loading = false;
     this.fly();
   }
 
@@ -141,57 +126,66 @@ export class EngineService {
       }
 
       this.canvas.addEventListener('mouseout', () => {
-        const translateVector = new Vector3(-this.branch.position.x, -this.branch.position.y + 2, 0);
-        const distance = translateVector.length();
+        if (!this.loading) {
+          const translateVector = new Vector3(-this.branch.position.x, -this.branch.position.y + 2, 0);
+          const distance = translateVector.length();
 
-        const direction = new Vector3(translateVector.x, translateVector.y, translateVector.z);
-        direction.normalize();
-        const deltaDistance = 0.2;
+          const direction = new Vector3(translateVector.x, translateVector.y, translateVector.z);
+          direction.normalize();
+          const deltaDistance = 0.2;
 
-        let i = 0;
-        this.scene.registerAfterRender(() => {
-          if (i++ * deltaDistance <= distance) {
-            this.branch.translate(direction, deltaDistance, Space.WORLD);
-          }
-        });
+          let i = 0;
+          this.scene.registerAfterRender(() => {
+            if (i++ * deltaDistance <= distance) {
+              this.branch.translate(direction, deltaDistance, Space.WORLD);
+            }
+          });
+        }
       });
 
       this.canvas.addEventListener('mousemove', () => {
-        this.nofly = true;
-        const offsetCanvasx = this.canvas.width / 200;
-        const offsetCanvasy = this.canvas.height / 200;
-        const x = (this.scene.pointerX / 100 - offsetCanvasx) / 2;
-        const y = (-this.scene.pointerY / 100 + offsetCanvasy + 3) / 1.5;
-        this.opener(x, y);
-        if (!this.open) {
-          if (this.branch.position.x > x && !this.leftoright) {
-            this.koodibril.rotate(new Vector3(0, 1, 0), Math.PI);
-            this.leftoright = true;
-          }
-          if (this.branch.position.x < x && this.leftoright) {
-            this.koodibril.rotate(new Vector3(0, 1, 0), Math.PI);
-            this.leftoright = false;
-          }
-          this.branch.position.x = x;
-          this.branch.position.y = y;
-        }
-        if (!this.timeout) {
-          this.timeout = true;
-          setTimeout(() => {
-            if (!this.open) {
-              this.nofly = false;
-              this.fly();
+        if (!this.loading) {
+          console.log(this.loading);
+          this.nofly = true;
+          const offsetCanvasx = this.canvas.width / 200;
+          const offsetCanvasy = this.canvas.height / 200;
+          const x = (this.scene.pointerX / 100 - offsetCanvasx) / 2;
+          const y = (-this.scene.pointerY / 100 + offsetCanvasy + 3) / 1.5;
+          this.opener(x, y);
+          if (!this.open) {
+            if (this.branch.position.x > x && !this.leftoright) {
+              this.koodibril.rotate(new Vector3(0, 1, 0), Math.PI);
+              this.leftoright = true;
             }
-            this.timeout = false;
-          }, 2000);
+            if (this.branch.position.x < x && this.leftoright) {
+              this.koodibril.rotate(new Vector3(0, 1, 0), Math.PI);
+              this.leftoright = false;
+            }
+            this.branch.position.x = x;
+            this.branch.position.y = y;
+          }
+          if (!this.timeout) {
+            this.timeout = true;
+            setTimeout(() => {
+              if (!this.open) {
+                this.nofly = false;
+                this.fly();
+              }
+              this.timeout = false;
+            }, 2000);
+          }
         }
       });
 
       this.canvas.addEventListener('wheel', event => {
         if (this.open) {
-          this.particle.stop();
+          this.loading = true;
           this.fly();
           this.flowers[0][0][0].start(false, 0.5);
+          this.bushes[0][0][1].start(false, 0.5);
+          this.bushes[1][0][1].start(false, 0.5);
+          this.bushes[2][0][1].start(false, 0.5);
+          this.bushes[3][0][1].start(false, 0.5);
           this.koodibrilAnim[1].stop();
           this.koodibrilAnim[0].start(true, 10);
         }
@@ -273,36 +267,32 @@ export class EngineService {
     const xOffsetl = flowerPos.x < 0 ? 0.1 : 0.5;
     if (flowerPos.x >= x - xOffsetr && flowerPos.x <= x + xOffsetl && flowerPos.y >= y - 0.5 && flowerPos.y <= y + 1 && !this.open) {
       this.open = true;
+      this.loading = true;
       this.goToFlower();
       this.flowers[0][0][1].start(false, 0.5);
-      this.createParticle();
+      this.bushes[0][0][0].start(false, 0.05);
+      this.bushes[1][0][0].start(false, 0.05);
+      this.bushes[2][0][0].start(false, 0.05);
+      this.bushes[3][0][0].start(false, 0.05).onAnimationEndObservable.add(() => {
+        this.loading = false;
+      });
     } else if (
       (flowerPos.x <= x - xOffsetr || flowerPos.x >= x + xOffsetl || flowerPos.y <= y - 0.5 || flowerPos.y >= y + 1) &&
       this.open
     ) {
-      this.particle.stop();
       this.flowers[0][0][0].start(false, 0.5);
+      this.bushes[0][0][0].stop();
+      this.bushes[1][0][0].stop();
+      this.bushes[2][0][0].stop();
+      this.bushes[3][0][0].stop();
+      this.bushes[0][0][1].start(false, 0.5);
+      this.bushes[1][0][1].start(false, 0.5);
+      this.bushes[2][0][1].start(false, 0.5);
+      this.bushes[3][0][1].start(false, 0.5);
       this.open = false;
       this.koodibrilAnim[1].stop();
       this.koodibrilAnim[0].start(true, 10);
     }
-  }
-
-  public createParticle(): void {
-    this.particle = new ParticleSystem('particles', 10000, this.scene);
-    this.particle.particleTexture = new Texture('../../content/assets/textures/flare.png', this.scene);
-    this.particle.emitter = this.flowers[0][1][0];
-    this.particle.minSize = 0.05;
-    this.particle.maxSize = 0.1;
-    this.particle.minLifeTime = 4.0;
-    this.particle.maxLifeTime = 4.0;
-    this.particle.emitRate = 500;
-    this.particle.blendMode = ParticleSystem.BLENDMODE_ONEONE;
-    this.particle.gravity = new Vector3(0, -10, 0);
-    this.particle.minEmitPower = 1;
-    this.particle.maxEmitPower = 4;
-    this.particle.updateSpeed = 1 / 60;
-    this.particle.start();
   }
 
   public goToFlower(): void {
@@ -422,73 +412,24 @@ export class EngineService {
   }
 
   public async seed(): Promise<void> {
-    this.shuffleMesh(this.trees);
-    const treeFront1 = this.trees[0].clone('', null);
-    const treeFront2 = this.trees[1].clone('', null);
-    const treeMiddle1 = this.trees[2].clone('', null);
-    const treeMiddle2 = this.trees[3].clone('', null);
-    const treeMiddle3 = this.trees[4].clone('', null);
-    const treeBack1 = this.trees[5].clone('', null);
-    const treeBack2 = this.trees[6].clone('', null);
-    const treeBack3 = this.trees[7].clone('', null);
-    const treeBack4 = this.trees[8].clone('', null);
-
-    treeFront1!.position.x = -3;
-    treeFront1!.position.z = 0;
-
-    treeFront2!.position.x = 4;
-    treeFront2!.position.z = 0;
-
-    treeMiddle1!.position.x = -6;
-    treeMiddle1!.position.z = 4;
-
-    treeMiddle2!.position.x = 3;
-    treeMiddle2!.position.z = 4;
-
-    treeMiddle3!.position.x = 5;
-    treeMiddle3!.position.z = 4;
-
-    treeBack1!.position.x = -4;
-    treeBack1!.position.z = 8;
-
-    treeBack2!.position.x = -3;
-    treeBack2!.position.z = 8;
-
-    treeBack3!.position.x = 2;
-    treeBack3!.position.z = 8;
-
-    treeBack4!.position.x = 5;
-    treeBack4!.position.z = 8;
-
-    this.forest.push(treeFront1!);
-    this.forest.push(treeFront2!);
-    this.forest.push(treeMiddle1!);
-    this.forest.push(treeMiddle2!);
-    this.forest.push(treeMiddle3!);
-    this.forest.push(treeBack1!);
-    this.forest.push(treeBack2!);
-    this.forest.push(treeBack3!);
-    this.forest.push(treeBack4!);
+    const random_tree = this.shuffleArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    await this.addtree(new Vector3(-3, 0, 0), false, random_tree[0]);
+    await this.addtree(new Vector3(4, 0, 0), false, random_tree[1]);
+    await this.addtree(new Vector3(-6, 0, 4), false, random_tree[2]);
+    await this.addtree(new Vector3(3, 0, 4), false, random_tree[3]);
+    await this.addtree(new Vector3(5, 0, 4), false, random_tree[4]);
+    await this.addtree(new Vector3(-4, 0, 8), false, random_tree[5]);
+    await this.addtree(new Vector3(-3, 0, 8), false, random_tree[6]);
+    await this.addtree(new Vector3(2, 0, 8), false, random_tree[7]);
+    await this.addtree(new Vector3(5, 0, 8), false, random_tree[8]);
 
     for (let z = 0; z <= 8; z = z + 4) {
-      this.shuffleMesh(this.bushes);
-      const cloneBush1 = this.bushes[0].clone('', null); // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-      const cloneBush2 = this.bushes[1].clone('', null); // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-      const cloneBush3 = this.bushes[2].clone('', null); // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
-      const cloneBush4 = this.bushes[3].clone('', null); // this.bushes[Math.floor(Math.random() * (4 - 1) + 1)];
+      const random = this.shuffleArray([1, 2, 3, 4]);
+      await this.addbush(new Vector3(-5, 0, z), false, random[0]);
+      await this.addbush(new Vector3(1, 0, z), false, random[1]);
+      await this.addbush(new Vector3(-1, 0, z), false, random[2]);
+      await this.addbush(new Vector3(5, 0, z), false, random[3]);
       await this.addflower(new Vector3(Math.random() * (1.5 - -1) + -1, 1.5, z), false);
-      cloneBush1!.position.x = -5;
-      cloneBush1!.position.z = z;
-      cloneBush2!.position.x = 1;
-      cloneBush2!.position.z = z;
-      cloneBush3!.position.x = -1;
-      cloneBush3!.position.z = z;
-      cloneBush4!.position.x = 5;
-      cloneBush4!.position.z = z;
-      this.forest.push(cloneBush1!);
-      this.forest.push(cloneBush2!);
-      this.forest.push(cloneBush3!);
-      this.forest.push(cloneBush4!);
     }
   }
 
@@ -509,34 +450,18 @@ export class EngineService {
   }
 
   public async addRow(delta: number): Promise<void> {
-    this.shuffleMesh(this.trees);
+    const random_tree = this.shuffleArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
     const newTrees = Math.floor(Math.random() * (4 - 2) + 2);
-    const tree_position = [-3, 4, -6, 3, 5, 2];
-    const position = this.shuffleArray(tree_position);
+    const position = this.shuffleArray([-3, 4, -6, 3, 5, 2]);
     for (let i = 0; i < newTrees; i++) {
-      const newTree = this.trees[i].clone('', null);
-      newTree!.position.x = position[i];
-      newTree!.position.z = delta === -1 ? 12 : -4;
-      this.forest.push(newTree!);
+      await this.addtree(new Vector3(position[i], 0, delta === -1 ? 12 : -4), delta === -1 ? false : true, random_tree[0]);
     }
-    this.shuffleMesh(this.bushes);
-    const cloneBush1 = this.bushes[0].clone('', null);
-    const cloneBush2 = this.bushes[1].clone('', null);
-    const cloneBush3 = this.bushes[2].clone('', null);
-    const cloneBush4 = this.bushes[3].clone('', null);
+    const random = this.shuffleArray([1, 2, 3, 4]);
     await this.addflower(new Vector3(Math.random() * (1.5 - -1) + -1, 1.5, delta === -1 ? 12 : -4), delta === -1 ? false : true);
-    cloneBush1!.position.x = -5;
-    cloneBush1!.position.z = delta === -1 ? 12 : -4;
-    cloneBush2!.position.x = 1;
-    cloneBush2!.position.z = delta === -1 ? 12 : -4;
-    cloneBush3!.position.x = -1;
-    cloneBush3!.position.z = delta === -1 ? 12 : -4;
-    cloneBush4!.position.x = 5;
-    cloneBush4!.position.z = delta === -1 ? 12 : -4;
-    this.forest.push(cloneBush1!);
-    this.forest.push(cloneBush2!);
-    this.forest.push(cloneBush3!);
-    this.forest.push(cloneBush4!);
+    await this.addbush(new Vector3(-5, 0, delta === -1 ? 12 : -4), delta === -1 ? false : true, random[0]);
+    await this.addbush(new Vector3(1, 0, delta === -1 ? 12 : -4), delta === -1 ? false : true, random[1]);
+    await this.addbush(new Vector3(-1, 0, delta === -1 ? 12 : -4), delta === -1 ? false : true, random[2]);
+    await this.addbush(new Vector3(5, 0, delta === -1 ? 12 : -4), delta === -1 ? false : true, random[3]);
   }
 
   public deleteRow(delta: number): void {
@@ -544,6 +469,12 @@ export class EngineService {
       if ((this.forest[i].position.z >= 11 && delta === 1) || (this.forest[i].position.z <= -3 && delta === -1)) {
         if (this.forest[i].name === 'flower') {
           delta === -1 ? this.flowers.splice(0, 1) : this.flowers.splice(3, 1);
+        }
+        if (this.forest[i].name === 'bush') {
+          delta === -1 ? this.bushes.splice(0, 1) : this.bushes.splice(9, 1);
+        }
+        if (this.forest[i].name === 'tree') {
+          delta === -1 ? this.trees.splice(0, 1) : this.trees.splice(this.trees.length, 1);
         }
         this.forest[i].dispose();
         this.forest.splice(i, 1);
@@ -563,5 +494,31 @@ export class EngineService {
     back ? this.flowers.unshift([flower.animationGroups, flower.meshes]) : this.flowers.push([flower.animationGroups, flower.meshes]);
     this.forest.push(flower.meshes[0]);
     return flower;
+  }
+
+  public async addbush(position: Vector3, back: boolean, mesh: number): Promise<ISceneLoaderAsyncResult> {
+    const bush = await SceneLoader.ImportMeshAsync('', '../../content/assets/models/', 'bush' + mesh.toString() + '.glb', this.scene);
+    bush.animationGroups[0].goToFrame(0);
+    bush.animationGroups[0].stop();
+    bush.meshes[0].scaling.scaleInPlace(2.5);
+    bush.meshes[0].rotate(new Vector3(0, 1, 0), position.x < 0 ? Math.PI * 1.5 : Math.PI / 2);
+    bush.meshes[0].position = position;
+    bush.meshes[0].name = 'bush';
+    back ? this.bushes.unshift([bush.animationGroups, bush.meshes]) : this.bushes.push([bush.animationGroups, bush.meshes]);
+    this.forest.push(bush.meshes[0]);
+    return bush;
+  }
+
+  public async addtree(position: Vector3, back: boolean, mesh: number): Promise<ISceneLoaderAsyncResult> {
+    const tree = await SceneLoader.ImportMeshAsync('', '../../content/assets/models/', 'tree' + mesh.toString() + '.glb', this.scene);
+    // tree.animationGroups[0].goToFrame(0);
+    // tree.animationGroups[0].stop();
+    tree.meshes[0].scaling.scaleInPlace(2.5);
+    tree.meshes[0].rotate(new Vector3(0, 1, 0), position.x < 0 ? Math.PI * 1.5 : Math.PI / 2);
+    tree.meshes[0].position = position;
+    tree.meshes[0].name = 'tree';
+    back ? this.trees.unshift([tree.animationGroups, tree.meshes]) : this.trees.push([tree.animationGroups, tree.meshes]);
+    this.forest.push(tree.meshes[0]);
+    return tree;
   }
 }
