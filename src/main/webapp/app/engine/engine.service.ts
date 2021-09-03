81e6e2e8d5ca760223b6dc13aeb5315e902d26c0
AlphaBril <megack79@gmail.com>
2021-09-03 20:45:57 +0200

    added sun and moon still some improvements to make

--- src/main/webapp/app/engine/engine.service.ts
+++ src/main/webapp/app/engine/engine.service.ts
@@ -21,6 +21,8 @@ import {
   StandardMaterial,
   ParticleSystem,
   Texture,
+  SpotLight,
+  Mesh,
 } from '@babylonjs/core';
 import '@babylonjs/loaders/glTF';
 import { GridMaterial } from '@babylonjs/materials';
@@ -70,6 +72,15 @@ interface Forest {
   bushes: Bushes,
 }
 
+interface Lights {
+  sun: SpotLight,
+  sunMesh: Mesh,
+  moon: SpotLight,
+  moonMesh: Mesh,
+  ambiant: HemisphericLight,
+  groundLight: GridMaterial
+}
+
 @Injectable({ providedIn: 'root' })
 export class EngineService {
   private canvas!: HTMLCanvasElement;
@@ -77,6 +88,7 @@ export class EngineService {
   private camera!: FlyCamera;
   private scene!: Scene;
   private move!: boolean;
+  private lights!: Lights;
 
   private koodibril!: AbstractMesh;
   private forest!: Forest;
@@ -94,6 +106,7 @@ export class EngineService {
   private bushes!: AssetContainer[];
   private pannel!: AssetContainer;
   private particle!: ParticleSystem;
+  private hour!: number;
 
   public constructor(private ngZone: NgZone, private windowRef: WindowRefService) {}
 
@@ -108,10 +121,11 @@ export class EngineService {
 
     // create a basic BJS Scene object
     this.scene = new Scene(this.engine);
-    this.scene.clearColor = new Color4(0, 0, 0, 0);
+    this.scene.clearColor = new Color4(1, 1, 1, 1);
+    this.scene.ambientColor = new Color3(1, 1, 1);
     this.scene.fogMode = Scene.FOGMODE_LINEAR;
     this.scene.fogStart = 4.0;
-    this.scene.fogEnd = 18.0;
+    this.scene.fogEnd = 20.0;
     this.scene.fogColor = new Color3(0.9, 0.9, 0.85);
 
     // create a FreeCamera, and set its position to (x:5, y:10, z:-20 )
@@ -119,16 +133,34 @@ export class EngineService {
 
     // target the camera to scene origin
     this.camera.setTarget(new Vector3(0, 2, 0));
-    // night light
-    const light = new HemisphericLight("DirectionalLight", new Vector3(0, 1, 0), this.scene);
+    this.lights = <Lights>{};
+    this.lights.ambiant = new HemisphericLight("DirectionalLight", new Vector3(0, 1, 0), this.scene);
+    this.lights.ambiant.intensity = 1;
+    this.lights.sun = new SpotLight("sunLight", Vector3.Zero(), new Vector3(0, -1, 0), Math.PI, 1, this.scene);
+    this.lights.moon = new SpotLight("moonLight", Vector3.Zero(), new Vector3(0, -1, 0), Math.PI, 1, this.scene);
+    const yellowMat = new StandardMaterial("yellowMat", this.scene);
+    const whiteMat = new StandardMaterial("whiteMat", this.scene);
+    yellowMat.emissiveColor = Color3.Yellow();
+    whiteMat.emissiveColor = Color3.White();
+    this.lights.sun.diffuse = Color3.Yellow();
+    this.lights.moon.diffuse = Color3.White();
+    this.lights.sunMesh = MeshBuilder.CreateSphere("sunMesh", {diameter: 1.5});
+    this.lights.moonMesh = MeshBuilder.CreateSphere("moonMesh", {diameter: 1.5});
+    this.lights.sunMesh.material = yellowMat;
+    this.lights.moonMesh.material = whiteMat;
+    this.lights.sun.parent = this.lights.sunMesh;
+    this.lights.moon.parent = this.lights.moonMesh;
+    this.lights.sunMesh.position = new Vector3(0, 10, 4);
+    this.lights.moonMesh.position = new Vector3(0, -10, 4);
+    this.hour = 5;
 
     const ground = MeshBuilder.CreateGround('ground', { width: 30, height: 30 });
-    const grid = new GridMaterial('groundMat', this.scene);
-    grid.majorUnitFrequency = 20;
-    grid.gridOffset = new Vector3(0, 0, 4);
-    grid.mainColor = new Color3(1, 1, 1);
-    grid.lineColor = new Color3(0, 0, 0);
-    ground.material = grid;
+    this.lights.groundLight = new GridMaterial('groundMat', this.scene);
+    this.lights.groundLight.majorUnitFrequency = 20;
+    this.lights.groundLight.gridOffset = new Vector3(0, 0, 4);
+    this.lights.groundLight.mainColor = new Color3(1, 1, 1);
+    this.lights.groundLight.lineColor = new Color3(0, 0, 0);
+    ground.material = this.lights.groundLight;
     ground.material.backFaceCulling = false;
     ground.checkCollisions = true;
 
@@ -268,6 +300,132 @@ export class EngineService {
     });
   }
 
+  public movestar(object: Mesh, from: Vector3, to: Vector3): void {
+    const frameRate = 10;
+    const ykeyFrames = [
+      {
+        frame: 0,
+        value: from.y,
+      },
+      {
+        frame: frameRate,
+        value: to.y,
+      },
+    ];
+    const zkeyFrames = [
+      {
+        frame: 0,
+        value: from.z,
+      },
+      {
+        frame: frameRate,
+        value: to.z,
+      },
+    ];
+
+    const ySlide = new Animation('ySlide', 'position.y', frameRate, Animation.ANIMATIONTYPE_FLOAT);
+    const zSlide = new Animation('zSlide', 'position.z', frameRate, Animation.ANIMATIONTYPE_FLOAT);
+
+    ySlide.setKeys(ykeyFrames);
+    zSlide.setKeys(zkeyFrames);
+    const animations = [zSlide, ySlide];
+    this.scene.beginDirectAnimation(object, animations, 0, frameRate, false, 2).onAnimationEndObservable.add(() => {
+      console.log(object.name);
+      console.log(object.position);
+    });
+  }
+
+  public day(delta: number): void {
+    if (delta === -1) {
+      this.hour = this.hour === 0 ? 9 : this.hour - 1;
+    } else {
+      this.hour = this.hour === 9 ? 0 : this.hour + 1;
+    }
+    console.log(this.hour);
+    switch (this.hour) {
+      case 0:
+        this.lights.ambiant.intensity = 0;
+        this.scene.fogColor = new Color3(0, 0, 0);
+        this.scene.clearColor = new Color4(0, 0, 0, 1);
+        this.lights.groundLight.mainColor = new Color3(0, 0, 0);
+        this.movestar(this.lights.sunMesh, this.lights.sunMesh.position, new Vector3(0, -10, 4));
+        this.movestar(this.lights.moonMesh, this.lights.moonMesh.position, new Vector3(0, 10, 4));
+      break;
+      case 1:
+        this.lights.ambiant.intensity = 0.2;
+        this.scene.fogColor = new Color3(0.2, 0.2, 0.2);
+        this.scene.clearColor = new Color4(0.2, 0.2, 0.2, 1);
+        this.lights.groundLight.mainColor = new Color3(0.2, 0.2, 0.2);
+        this.movestar(this.lights.sunMesh, this.lights.sunMesh.position, new Vector3(0, -10, 8));
+        this.movestar(this.lights.moonMesh, this.lights.moonMesh.position, new Vector3(0, 10, -8));
+      break;
+      case 2:
+        this.lights.ambiant.intensity = 0.4;
+        this.scene.fogColor = new Color3(0.4, 0.4, 0.4);
+        this.scene.clearColor = new Color4(0.4, 0.4, 0.4, 1);
+        this.lights.groundLight.mainColor = new Color3(0.4, 0.4, 0.4);
+        this.movestar(this.lights.sunMesh, this.lights.sunMesh.position, new Vector3(0, -4, 12));
+        this.movestar(this.lights.moonMesh, this.lights.moonMesh.position, new Vector3(0, 4, -12));
+      break;
+      case 3:
+        this.lights.ambiant.intensity = 0.6;
+        this.scene.fogColor = new Color3(0.6, 0.6, 0.6);
+        this.scene.clearColor = new Color4(0.6, 0.6, 0.6, 1);
+        this.lights.groundLight.mainColor = new Color3(0.6, 0.6, 0.6);
+        this.movestar(this.lights.sunMesh, this.lights.sunMesh.position, new Vector3(0, 1, 12));
+        this.movestar(this.lights.moonMesh, this.lights.moonMesh.position, new Vector3(0, -1, -12));
+      break;
+      case 4:
+        this.lights.ambiant.intensity = 0.8;
+        this.scene.fogColor = new Color3(0.8, 0.8, 0.8);
+        this.scene.clearColor = new Color4(0.8, 0.8, 0.8, 1);
+        this.lights.groundLight.mainColor = new Color3(0.8, 0.8, 0.8);
+        this.movestar(this.lights.sunMesh, this.lights.sunMesh.position, new Vector3(0, 4, 12));
+        this.movestar(this.lights.moonMesh, this.lights.moonMesh.position, new Vector3(0, -4, -12));
+      break;
+      case 5:
+        this.lights.ambiant.intensity = 1;
+        this.scene.fogColor = new Color3(0.9, 0.9, 0.85);
+        this.scene.clearColor = new Color4(1, 1, 1, 1);
+        this.lights.groundLight.mainColor = new Color3(1, 1, 1);
+        this.movestar(this.lights.sunMesh, this.lights.sunMesh.position, new Vector3(0, 10, 4));
+        this.movestar(this.lights.moonMesh, this.lights.moonMesh.position, new Vector3(0, -10, 4));
+      break;
+      case 6:
+        this.lights.ambiant.intensity = 0.8;
+        this.scene.fogColor = new Color3(0.8, 0.8, 0.8);
+        this.scene.clearColor = new Color4(0.8, 0.8, 0.8, 1);
+        this.lights.groundLight.mainColor = new Color3(0.8, 0.8, 0.8);
+        this.movestar(this.lights.sunMesh, this.lights.sunMesh.position, new Vector3(0, 4, -12));
+        this.movestar(this.lights.moonMesh, this.lights.moonMesh.position, new Vector3(0, -4, 12));
+      break;
+      case 7:
+        this.lights.ambiant.intensity = 0.6;
+        this.scene.fogColor = new Color3(0.6, 0.6, 0.6);
+        this.scene.clearColor = new Color4(0.6, 0.6, 0.6, 1);
+        this.lights.groundLight.mainColor = new Color3(0.6, 0.6, 0.6);
+        this.movestar(this.lights.sunMesh, this.lights.sunMesh.position, new Vector3(0, 1, -12));
+        this.movestar(this.lights.moonMesh, this.lights.moonMesh.position, new Vector3(0, -1, 12));
+      break;
+      case 8:
+        this.lights.ambiant.intensity = 0.4;
+        this.scene.fogColor = new Color3(0.4, 0.4, 0.4);
+        this.scene.clearColor = new Color4(0.4, 0.4, 0.4, 1);
+        this.lights.groundLight.mainColor = new Color3(0.4, 0.4, 0.4);
+        this.movestar(this.lights.sunMesh, this.lights.sunMesh.position, new Vector3(0, -4, -12));
+        this.movestar(this.lights.moonMesh, this.lights.moonMesh.position, new Vector3(0, 4, 12));
+      break;
+      case 9:
+        this.lights.ambiant.intensity = 0.2;
+        this.scene.fogColor = new Color3(0.2, 0.2, 0.2);
+        this.scene.clearColor = new Color4(0.2, 0.2, 0.2, 1);
+        this.lights.groundLight.mainColor = new Color3(0.2, 0.2, 0.2);
+        this.movestar(this.lights.sunMesh, this.lights.sunMesh.position, new Vector3(0, -10, -8));
+        this.movestar(this.lights.moonMesh, this.lights.moonMesh.position, new Vector3(0, 10, 8));
+      break;
+    }
+  }
+
   public reset(): void {
     if (!this.loading) {
       this.loading = false;
@@ -342,6 +500,7 @@ export class EngineService {
         this.addRow(delta);
         let rollOver: any;
         let toMove: any;
+        this.day(delta);
         for (let i = 0; i < 12; i++) {
           switch (i) {
             case 0:
