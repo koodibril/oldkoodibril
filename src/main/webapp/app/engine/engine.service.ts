2ae44f85a2408f6fd43b73cef71077d2e64e30e3
addy-dclxvi <addy4rock@gmail.com>
2022-02-24 19:17:23 +0100

    on pc, single click and double tap for phones

--- src/main/webapp/app/engine/engine.service.ts
+++ src/main/webapp/app/engine/engine.service.ts
@@ -1,6 +1,6 @@
 import { WindowRefService } from './../services/window-ref.service';
 import { ElementRef, Injectable, NgZone } from '@angular/core';
-import { Engine, Scene, MeshBuilder, Color4, Vector3, Color3, FlyCamera, PointerEventTypes, DeviceSourceManager } from '@babylonjs/core';
+import { Engine, Scene, MeshBuilder, Color4, Vector3, Color3, FlyCamera, PointerEventTypes, Matrix } from '@babylonjs/core';
 import '@babylonjs/loaders/glTF';
 import { GridMaterial } from '@babylonjs/materials';
 import { ForestActions, Forest } from './actions/forest.service';
@@ -62,6 +62,7 @@ export class EngineService {
     const loadingScreen = new CustomLoadingScreen(this.canvas);
     this.engine.loadingScreen = loadingScreen;
     this.engine.displayLoadingUI();
+    this.engine.setHardwareScalingLevel(1 / window.devicePixelRatio);
     this.loadingState.subscribe(value => {
       this.engine.loadingUIText = value;
     });
@@ -103,22 +104,68 @@ export class EngineService {
     await this.animationsActions.initiateAnimation();
     this.koodibril = this.animationsActions.koodibril;
 
-    new DeviceSourceManager(this.scene.getEngine()).onDeviceConnectedObservable.add(device => {
-      this.device = device.deviceType;
-    });
-
-    // if (this.device === 2) {
-    //   this.guiAction = new GuiActions(this.scene, this.camera, this.engine, this.lights, this.forest);
-    //   this.guiAction.instantiatePannelGui();
-    // }
-
     this.textActions = new textActions(this.scene, this.canvas, this.appName);
+
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+        navigator.userAgent
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(-|_)|g1 u|g560|gene|gf-5|g-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd-(m|p|t)|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c(-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac( |-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c(-|0|1)|47|mc|nd|ri)|sgh-|shar|sie(-|m)|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel(i|m)|tim-|t-mo|to(pl|sh)|ts(70|m-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(
+        navigator.userAgent.substr(0, 4)
+      )
+    ) {
+      this.device = 2;
+      // this.camera.position = new Vector3(0, 4, -7);
+    } else {
+      this.device = 1;
+    }
 
     this.timeout = false;
     this.open = false;
     this.position = 0;
     this.loadingState.next('');
     this.engine.loadingUIBackgroundColor = 'rgb(1, 1, 1, 0.7)';
+  }
+
+  // this class use vertex to get the position on canvas in px of an object rendered
+  public getPos(x: number, y: number): void {
+    const temp = new Vector3();
+    const vertices = this.textActions.bottomText.getBoundingInfo().boundingBox.vectorsWorld;
+    const viewport = this.camera.viewport.toGlobal(this.engine.getRenderWidth(), this.engine.getRenderHeight());
+    let minX = 1e10,
+      minY = 1e10,
+      maxX = -1e10,
+      maxY = -1e10;
+    for (const vertex of vertices) {
+      Vector3.ProjectToRef(vertex, Matrix.IdentityReadOnly, this.scene.getTransformMatrix(), viewport, temp);
+
+      if (minX > temp.x) {
+        minX = temp.x;
+      }
+      if (maxX < temp.x) {
+        maxX = temp.x;
+      }
+      if (minY > temp.y) {
+        minY = temp.y;
+      }
+      if (maxY < temp.y) {
+        maxY = temp.y;
+      }
+    }
+    const ofstX = this.canvas.offsetLeft,
+      ofstY = this.canvas.offsetTop;
+    // const htmlElem = document.getElementById('htlmElem');
+    // htmlElem.style.left = (minX + ofstX) + "px";
+    // htmlElem.style.top = (minY + ofstY) + "px";    for testing purpose
+    // htmlElem.style.width = (maxX - minX) + "px";
+    // htmlElem.style.height = (maxY - minY) + "px";
+    const left = minX + ofstX;
+    const top = minY + ofstY;
+    const width = maxX - minX + left;
+    const height = maxY - minY + top;
+    if (x < left || x > width || y < top || y > height) {
+      this.opener(0, 0);
+    }
   }
 
   // class that is called outside of the rendering for listeners
@@ -145,9 +192,16 @@ export class EngineService {
             this.wheel(pointerInfo.event);
             break;
           case PointerEventTypes.POINTERTAP:
+            if (!this.move && !this.loading && this.device === 1) {
+              if (this.open) {
+                this.getPos(pointerInfo.event.clientX, pointerInfo.event.clientY);
+              } else {
+                this.opener(this.forest.flowers.front.meshe.position.x, this.forest.flowers.front.meshe.position.y);
+              }
+            }
             break;
           case PointerEventTypes.POINTERDOUBLETAP:
-            if (!this.move && !this.loading) {
+            if (!this.move && !this.loading && this.device === 2) {
               if (this.open) {
                 this.opener(0, 0);
               } else {
